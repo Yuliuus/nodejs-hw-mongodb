04cdfaa7dcdda43ffe225bd3d34404597c1e739b
Yuliuus <yuliiamarkovska@gmail.com>
2024-09-24 17:51:47 +0300

    initial commit

--- src/scripts/getAllContacts.js
+++
@@ -1,9 +0,0 @@
-import { readContacts } from "../utils/readContacts.js";
-
-
-export const getAllContacts = async () => {
-    const data = await readContacts();
-    return data;
-};
-
-console.log(await getAllContacts());
