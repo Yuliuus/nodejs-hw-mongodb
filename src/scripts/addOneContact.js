6eb7164ab9fed502cd3968a36651df72c7b05c1e
Yuliuus <154925021+Yuliuus@users.noreply.github.com>
2024-09-24 17:57:38 +0300

    Revert "initial commit"

---
+++ src/scripts/addOneContact.js
@@ -0,0 +1,12 @@
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const addOneContact = async () => {
+    const contactList = await readContacts();
+    const newContact = createFakeContact();
+    contactList.push(newContact);
+    await writeContacts(contactList);
+};
+
+addOneContact();
---
+++ src/scripts/getAllContacts.js
@@ -0,0 +1,9 @@
+import { readContacts } from "../utils/readContacts.js";
+
+
+export const getAllContacts = async () => {
+    const data = await readContacts();
+    return data;
+};
+
+console.log(await getAllContacts());
